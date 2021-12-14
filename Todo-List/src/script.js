f58bf051346773dc1aa2db3a62007dd3ac2d457d
IUOAFRALE <zakaria.elarfaoui2105@gmail.com>
2021-12-14 12:03:06 +0100

    add task form pop up

---
+++ Todo-List/src/script.js
@@ -0,0 +1,18 @@
+$(document).ready(function()
+{
+
+    $(".new-task").click(function()
+    {
+
+        $(".task-form").show(300);
+
+    });
+
+    $(".close-task-form").click(function()
+    {
+
+        $(".task-form").hide(300);
+
+    });
+
+});
